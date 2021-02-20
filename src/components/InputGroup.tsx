881e56b438a8b72b400e6729b173e33ac82d951f
ChandrakantPal <sanjay161097@gmail.com>
2021-02-21 01:51:27 +0530

    focus classes changed

--- src/components/InputGroup.tsx
+++ src/components/InputGroup.tsx
@@ -19,7 +19,7 @@ const InputGroup: React.FC<InputGroupProps> = ({
     <div className={className}>
       <input
         type={type}
-        className="w-full p-3 transition duration-200 bg-gray-900 border border-gray-300 rounded outline-none focus:bg-gray-200 hover:bg-gray-200 hover:text-black"
+        className="w-full p-3 transition duration-200 bg-gray-900 border border-gray-300 rounded outline-none focus:bg-gray-800 focus:text-blue-300 focus:border-blue-300 hover:bg-gray-800 hover:text-black"
         placeholder={placeholder}
         value={value}
         onChange={(e) => setValue(e.target.value)}
