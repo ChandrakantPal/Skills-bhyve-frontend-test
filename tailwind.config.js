85a1c4cc1e843002fdd94529ccf45597909ed765
ChandrakantPal <sanjay161097@gmail.com>
2021-02-20 18:20:07 +0530

    container components css

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,5 +7,18 @@ module.exports = {
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    function ({ addComponents }) {
+      addComponents({
+        '.container': {
+          width: '100%',
+          marginLeft: 'auto',
+          marginRight: 'auto',
+          '@screen sm': { maxWidth: '640px' },
+          '@screen md': { maxWidth: '768px' },
+          '@screen lg': { maxWidth: '975px' },
+        },
+      })
+    },
+  ],
 }
